56ffe8e584100f66547f0eac063030e6ed3d11a0
Sean Kim <sean.sihun.kim@gmail.com>
2020-09-23 21:43:59 +0000

    Word search with edge casing in test file

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,39 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+    it("should return true if the word is present", function() {
+      const result = wordSearch([
+        ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+        ['S', 'E', 'I', 'G', 'F', 'E', 'L', 'D'],
+        ['Y', 'F', 'C', 'E', 'Q', 'U', 'A', 'L'],
+        ['H', 'M', 'J', 'O', 'E', 'V', 'R', 'G'],
+        ['W', 'H', 'C', 'R', 'Y', 'E', 'R', 'L'],
+        ['B', 'F', 'R', 'G', 'N', 'E', 'Y', 'B'],
+        ['U', 'B', 'T', 'E', 'A', 'P', 'A', 'I'],
+        ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+        ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+      ], 'GEORGE')
+  
+      assert.isTrue(result);
+    });
+    it("should return false if the word array is empty", function() {
+      const result = wordSearch([], 'GEORGE')
+  
+      assert.isFalse(result);
+    });
+    it("should return true if there is no word", function() {
+      const result = wordSearch([        
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'G', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'E', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'O', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'R', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'G', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'E', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L']
+    ], '')
+  
+      assert.isTrue(result);
+    });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,49 @@
 const wordSearch = (letters, word) => { 
+    /*
+    1. First look horizontally
+    2. If word not found horizontally, look vertically
+    3. 
+    */
+    if (letters.length === 0) {
+        return false;
+    };
+
+
+    if (word === undefined) {
+        return false;
+    };
+
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
+    };
+
+    letters = transpose(letters);
+    const verticalJoin = letters.map(ls => ls.join(''));
+    for (l of verticalJoin) {
+        if (l.includes(word)) return true
     }
-}
+    
+    return false;
+
+};
+
+const transpose = function(matrix) {
+    // Put your solution here
+    const rows = matrix.length;
+    const columns = matrix[0].length;
+    const output = []; 
+  
+    for (let i = 0; i < columns; i++) {
+      output[i] = Array(rows);
+    }
+    for (let j = 0; j < rows; j++) {
+      for (let i = 0; i < columns; i++) {
+      output[i][j] = matrix[j][i]
+      }
+    }
+    return output;
+  };
 
 module.exports = wordSearch
+
